5722f786b30a2f76738877b8636d39ef0b82ba15
JasonYou <qingwei0713@163.com>
2020-11-26 15:05:03 +0800

    feature: init base code

---
+++ src/helpers/url.ts
@@ -0,0 +1,11 @@
+import { isDate, isObject } from './util'
+
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  return ''
+}
---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+}
+
+export interface AxiosRequest {}
